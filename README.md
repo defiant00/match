c1735a8640a0472160d46038ea893aa1da6ade55
defiant00 <defiant00@gmail.com>
2022-07-05 18:40:39 -0700

    numbers and repeat

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # match
+
 Text matching and replacement.
+
+---
+
+1. Goal is more readable and flexible text processing than regexes
+2. Define patterns and processors
+3. Variables are scoped to the parent that they are defined in
+4. String (capture) and numeric (counter) variables
+5. Assignment using `:` or `=`? `variable = pattern/value/etc`
+6. Literal strings are quoted using `'` or `"`, quotes are escaped in strings by doubling. `'You probably shouldn''t...'`
+
+## Syntax
+
+### Constants
+
+* `any` - matches any single character
+* `line-start` - matches the start of the line
+* `line-end` - matches the end of the line
+* `newline` - matches the various newline combinations `\r\n`, `\r`, `\n`
+
+### Pattern Functions
+
+* `repeat pattern` - repeat the pattern 0 or more times
+* `repeat num pattern` - repeat the pattern `num` times
+* `repeat min max pattern` - repeat the pattern `min` to `max` times, you can use `or-more` for `max` for an unbound maximum
+* `repeat-lazy` - same parameters as `repeat`
+* `or pattern1 pattern2 ...` - accept the first matching pattern in the specified order
+* `followed-by pattern` - perform positive lookahead
+* `not-followed-by pattern` - perform negative lookahead
+* `preceded-by pattern` - perform positive lookbehind
+* `not-predeced-by pattern` - perform negative lookbehind
+
+### Processor Functions
+
+* tbd
+
+---
+
+## Old Notes
+
+### Pattern Commands
+* `pattern` - define a pattern
+* `processor` - define a processor
+* `var` - define variables
+* `set` - takes a variable and a value to set it to
+* `+, -, *, /, %` - basic math
+* `length` - takes a string and returns the length
+* `matches` - takes a pattern and string and returns the number of matches
+* `number` - takes a string and tries to convert it to a number
+
+### Processor Commands
+* `->` - define a processor step
+* repeat - takes a string and number and repeats the string that many times
+* some sort of conditional output
+* nested processors
--- text_match.js
+++ text_match.js
@@ -1,7 +1,6 @@
 class Functions {
-    static zero_or_more = 'any';
-    static zero_or_one = 'optional';
-    static one_or_more = 'some';
+    static repeat = 'repeat';
+    static repeat_lazy = 'repeat-lazy';
 
     static or = 'or';
 
@@ -9,10 +8,14 @@ class Functions {
     static negative_lookahead = 'not-followed-by';
     static lookbehind = 'preceded-by';
     static negative_lookbehind = 'not-preceded-by';
+}
 
-    static std_line_start = 'line-start';
-    static std_line_end = 'line-end';
-    static std_newline = 'newline';
+class Constants {
+    static any = 'any';
+    static line_start = 'line-start';
+    static line_end = 'line-end';
+    static newline = 'newline';
+    static or_more = 'or-more';
 }
 
 class TextMatcher {
@@ -47,75 +50,110 @@ class TextMatcher {
 
     static regexString(array) {
         let val = '';
-        let prop = '';
-        if (array[0] && array[0].type === Token.Identifier) {
-            prop = array[0].value;
-        }
-        switch (prop) {
-            case Functions.or:
-            case Functions.zero_or_more:
-            case Functions.one_or_more:
-            case Functions.zero_or_one:
-                val += '(?:';
-                break;
-            case Functions.lookahead:
-                val += '(?=';
-                break;
-            case Functions.negative_lookahead:
-                val += '(?!';
-                break;
-            case Functions.lookbehind:
-                val += '(?<=';
-                break;
-            case Functions.negative_lookbehind:
-                val += '(?<!';
-                break;
-        }
+        let [func, fnStart, fnEnd, startIndex] = TextMatcher.regexFuncValues(array);
+        val += fnStart;
         let first = true;
-        for (let item of array) {
-            if (!first) {
-                if (prop === Functions.or) {
+        for (let i = startIndex; i < array.length; i++) {
+            let item = array[i];
+            if (first) {
+                first = false;
+            } else {
+                if (func === Functions.or) {
                     val += '|';
                 }
             }
             if (Array.isArray(item)) {
                 val += TextMatcher.regexString(item);
-                first = false;
             } else if (item.type === Token.Identifier) {
                 val += TextMatcher.stdLibraryRegex(item.value);
             } else if (item.type === Token.String) {
-                val += TextMatcher.escapeString(item.value.slice(1, item.value.length - 1));
-                first = false;
+                val += TextMatcher.escapeString(item.value);
             }
         }
-        switch (prop) {
-            case Functions.lookahead:
-            case Functions.negative_lookahead:
-            case Functions.lookbehind:
-            case Functions.negative_lookbehind:
-                val += ')';
-                break;
-            case Functions.or:
-                val += ')';
-                break;
-            case Functions.zero_or_more:
-                val += ')*';
-                break;
-            case Functions.one_or_more:
-                val += ')+';
-                break;
-            case Functions.zero_or_one:
-                val += ')?';
-                break;
-        }
+        val += fnEnd;
         return val;
+    }
+
+    static regexFuncValues(array) {
+        let funcName = '';
+        let start = '';
+        let end = '';
+        let startingIndex = 0;
+        if (array[0] && array[0].type === Token.Identifier) {
+            funcName = array[0].value;
+
+            switch (funcName) {
+                case Functions.or:
+                    start = '(?:';
+                    end = ')';
+                    startingIndex = 1;
+                    break;
+                case Functions.repeat:
+                case Functions.repeat_lazy:
+                    start = '(?:';
+                    end = ')*';
+                    startingIndex = 1;
+                    let v1 = array[1];
+                    if (v1 && v1.type === Token.Number) {
+                        let v2 = array[2];
+                        if (v2 && v2.type === Token.Number) {
+                            if (v1.value === 0 && v2.value === 1) {
+                                end = ')?';
+                            } else {
+                                end = '){' + v1.value + ',' + v2.value + '}';
+                            }
+                            startingIndex = 3;
+                        } else if (v2 && v2.type === Token.Identifier && v2.value === Constants.or_more) {
+                            if (v1.value === 0) {
+                                end = ')*';
+                            } else if (v1.value === 1) {
+                                end = ')+';
+                            } else {
+                                end = '){' + v1.value + ',}';
+                            }
+                            startingIndex = 3;
+                        } else {
+                            end = '){' + array[1].value + '}';
+                            startingIndex = 2;
+                        }
+                    }
+                    if (funcName === Functions.repeat_lazy) {
+                        end += '?';
+                    }
+                    break;
+                case Functions.lookahead:
+                    start = '(?=';
+                    end = ')';
+                    startingIndex = 1;
+                    break;
+                case Functions.negative_lookahead:
+                    start = '(?!';
+                    end = ')';
+                    startingIndex = 1;
+                    break;
+                case Functions.lookbehind:
+                    start = '(?<=';
+                    end = ')';
+                    startingIndex = 1;
+                    break;
+                case Functions.negative_lookbehind:
+                    start = '(?<!';
+                    end = ')';
+                    startingIndex = 1;
+                    break;
+            }
+        }
+
+        // function name, starting string, ending string, starting array value index
+        return [funcName, start, end, startingIndex];
     }
 
     static stdLibraryRegex(identifier) {
         switch (identifier) {
-            case Functions.std_line_start: return '^';
-            case Functions.std_line_end: return '$';
-            case Functions.std_newline: return '(?:\\r\\n|\\r|\\n)';
+            case Constants.any: return '.';
+            case Constants.line_start: return '^';
+            case Constants.line_end: return '$';
+            case Constants.newline: return '(?:\\r\\n|\\r|\\n)';
         }
         return '';
     }
@@ -139,7 +177,7 @@ class TextMatcher {
                 val += ' ';
             }
             if (Array.isArray(item)) {
-                val += this.arrayString(item);
+                val += TextMatcher.arrayString(item);
             } else {
                 val += item;
             }
@@ -183,16 +221,23 @@ class TextMatcher {
                     if (value[currentIndex] !== val) {
                         this.errors.push('unclosed ' + val);
                     }
-                    this.tokens.push(new Token(Token.String, value.slice(startIndex, ++currentIndex)));
+                    this.tokens.push(new Token(Token.String, value.slice(startIndex + 1, currentIndex++)));
                     startIndex = currentIndex;
                     break;
                 default:
-                    if (this.isStartingIdentifierChar(val)) {
+                    if (TextMatcher.isStartingIdentifierChar(val)) {
                         currentIndex++;
-                        while (currentIndex < value.length && this.isIdentifierChar(value[currentIndex])) {
+                        while (currentIndex < value.length && TextMatcher.isIdentifierChar(value[currentIndex])) {
                             currentIndex++;
                         }
                         this.tokens.push(new Token(Token.Identifier, value.slice(startIndex, currentIndex)));
+                        startIndex = currentIndex;
+                    } else if (TextMatcher.isDigit(val)) {
+                        currentIndex++;
+                        while (currentIndex < value.length && TextMatcher.isDigit(value[currentIndex])) {
+                            currentIndex++;
+                        }
+                        this.tokens.push(new Token(Token.Number, Number(value.slice(startIndex, currentIndex))));
                         startIndex = currentIndex;
                     } else {
                         startIndex = ++currentIndex;
@@ -202,12 +247,16 @@ class TextMatcher {
         }
     }
 
-    isStartingIdentifierChar(c) {
+    static isStartingIdentifierChar(c) {
         return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c === '_';
     }
 
-    isIdentifierChar(c) {
-        return this.isStartingIdentifierChar(c) || (c >= '0' && c <= '9') || c === '.' || c === '-';
+    static isIdentifierChar(c) {
+        return TextMatcher.isStartingIdentifierChar(c) || (c >= '0' && c <= '9') || c === '.' || c === '-';
+    }
+
+    static isDigit(c) {
+        return c >= '0' && c <= '9';
     }
 
     parse(index, parent) {
@@ -242,6 +291,7 @@ class Token {
     static RightParen = 1;
     static Identifier = 2;
     static String = 3;
+    static Number = 4;
 
     constructor(type, value) {
         this.type = type;
@@ -252,6 +302,7 @@ class Token {
         switch (this.type) {
             case Token.LeftParen: return '(';
             case Token.RightParen: return ')';
+            case Token.String: return '"' + this.value + '"';
             default: return this.value;
         }
     }
